f5dd19979a6f7a6ae64e1159017d9fbfb90d6e54
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-11-30 01:23:28 +0200

    add: task 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,29 @@
 package com.company;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
+        taskOne();
+        taskTwo();
+        taskThree();
+        taskFour();
+    }
+
+    private static void taskOne() {
         List<Book> firstTask = Book.getBookList().stream()
                 .filter(book -> book.getPrice().compareTo(BigDecimal.valueOf(200)) <= 0)
                 .collect(Collectors.toList());
 
+        System.out.println("\nTASK 1:");
+        firstTask.forEach(book -> System.out.println(book.toString()));
+    }
+
+    private static void taskTwo() {
         AtomicInteger count = new AtomicInteger();
         List<Book> secondTask = Book.getBookList().stream()
                 .peek(book -> {
@@ -21,27 +32,30 @@ public class Main {
                 })
                 .collect(Collectors.toList());
 
-
-        System.out.println("\nfirstTask:");
-
-        for (Book book : firstTask) {
-            System.out.println(book.toString());
-        }
-
-        System.out.println("\nsecondTask:\ncount = " + count);
-
-        for (Book book : secondTask) {
-            System.out.println(book.toString());
-        }
-
-        System.out.println("\nthirdTask:");
-
-        for (Book book : secondTask) {
-            System.out.println(book.getTitle());
-        }
+        System.out.println("\nTASK 2:\ncount = " + count);
+        secondTask.forEach(book -> System.out.println(book.toString()));
     }
 
-    public static int add(int x, int y) {
-        return x + y;
+    private static void taskThree() {
+        System.out.println("\nTASK 3:");
+        Book.getBookList().forEach(book -> System.out.println(book.getTitle()));
+    }
+
+    private static void taskFour() {
+        Set<String> authorUniqueList = Book.getBookList().stream()
+                .map(Book::getAuthor)
+                .collect(Collectors.toSet());
+
+        Map<String, List<Book>> fourthTask = new HashMap<>();
+
+        authorUniqueList.forEach(author -> {
+            fourthTask.put(author,
+                    Book.getBookList().stream()
+                            .filter(book -> book.getAuthor().equals(author))
+                            .collect(Collectors.toList()));
+        });
+
+        System.out.println("\nTASK 4:");
+        fourthTask.entrySet().forEach(System.out::println);
     }
 }
